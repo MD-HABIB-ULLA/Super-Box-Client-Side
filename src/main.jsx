32c66d34b6aaa4e16c0725ee2e46204417df1ef4
habib ulla <habibulla1278@gmail.com>
2024-09-14 12:28:41 +0600

    tanstack quary added

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,19 @@ import { RouterProvider } from "react-router-dom";
 import router from "./Router/Routes/router";
 import AuthProvider from "./Provider/AuthProvider";
 import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+
+
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <Toaster />
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Toaster />
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
