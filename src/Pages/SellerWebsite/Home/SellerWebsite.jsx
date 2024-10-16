23d19ee3c081174dacbfbb3e27cc9c34b6f72770
habib ulla <habibulla1278@gmail.com>
2024-10-17 01:12:26 +0600

    sevices page added

--- src/Pages/SellerWebsite/Home/SellerWebsite.jsx
+++ src/Pages/SellerWebsite/Home/SellerWebsite.jsx
@@ -23,7 +23,7 @@ const SellerWebsite = () => {
       />
 
       {products?.length !== 0 && (
-        <section className="py-16 px-4 sm:px-6 lg:px-8 bg-gradient-to-b from-gray-50 to-white">
+        <section className="py-16 px-4 sm:px-6 lg:px-8 ">
           <div className="max-w-7xl mx-auto">
             <div className="text-center mb-12">
               <h2 className="text-3xl sm:text-4xl font-extrabold text-gray-900 mb-4">
