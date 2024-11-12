bf4e6960b697dbd7bb28b10fa2b0ae89dca03ade
habib ulla <habibulla1278@gmail.com>
2024-11-12 22:33:08 +0600

    On branch main

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:3000", 
-  // baseURL: "https://super-box-server-sandy.vercel.app",
-});
+  // baseURL: "http://localhost:3000", 
+//   // baseURL: "https://super-box-server-sandy.vercel.app",
+// });
 
 const useAxiosPublic = () => {
   return axiosPublic;
